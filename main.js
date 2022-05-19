13e77530d39105c7ba18b34efc347fb2ba78b19f
urlastway <andriy.semenchenko13@gmail.com>
2022-05-20 00:14:43 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var app = new Vue(
+    {
+        el: `#root`,
+        data: {
+            massage: `Hello Word`,
+        }
+    }
+)
+
+var app = new Vue(
+    {
+        el:`#img`,
+        data: {
+            img: //media-cdn.tripadvisor.com/media/photo-s/15/a4/9b/77/legacy-hotel-at-img-academy.jpg
+        }
+    }
+)
